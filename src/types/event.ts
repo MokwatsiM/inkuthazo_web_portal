f2f13ddb1f292571fa89a525bdffd1548914d341
MokwatsiM <motebangmokwatsi0@gmail.com>
2025-03-10 14:05:31 +0300

    chore: event type

---
+++ src/types/event.ts
@@ -0,0 +1,26 @@
+import { Timestamp } from "firebase/firestore";
+
+export type EventType = "meeting" | "event" | "reminder";
+export type RecurrenceType = "none" | "monthly-date" | "monthly-day";
+
+export interface Event {
+  id: string;
+  title: string;
+  description: string;
+  start: Timestamp;
+  end: Timestamp;
+  allDay: boolean;
+  created_by: string;
+  created_at: Timestamp;
+  updated_at: Timestamp;
+  color?: string;
+  type: EventType;
+  recurrence?: {
+    type: RecurrenceType;
+    dayOfMonth?: number;
+    weekNumber?: number;
+    dayOfWeek?: number;
+  };
+}
+
+export type EventInput = Omit<Event, "id" | "created_at" | "updated_at">;
